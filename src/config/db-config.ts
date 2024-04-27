94380c8778508a279e1b7f648efee69955e8aaad
khalid <khalid@grype.ca>
2024-04-27 12:36:24 +0600

    dynamically setting db-config path

--- src/config/db-config.ts
+++ src/config/db-config.ts
@@ -1,19 +1,20 @@
 import { DataSource } from "typeorm";
+import path from "node:path";
 import dotenv from "dotenv";
 dotenv.config();
 
 const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ["src/entities/**/*.ts"],
-  migrations: ["src/migrations/**/*.ts"],
-  logging: true,
-  synchronize: false,
-  migrationsTableName: "custom_migration_table",
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [path.join(__dirname, "../entities/**/*.{ts,js}")],
+    migrations: [path.join(__dirname, "../migrations/**/*.{ts,js}")],
+    logging: true,
+    synchronize: false,
+    migrationsTableName: "custom_migration_table",
 });
 
 export default AppDataSource;
