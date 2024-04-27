6719f25a78ebb51efe232f4e48e3f57432e5b732
dev-khalid <khalid.hossain.akash@gmail.com>
2024-04-27 15:47:24 +0600

    basic api testing

--- src/route/__tests__/customer.test.ts
+++ src/route/__tests__/customer.test.ts
@@ -1,25 +1,25 @@
-import server from "../..";
 import request from "supertest";
 
 // group test using describe
 describe("POST /register", () => {
-    it("returns status code 201 if first name is passed", async () => {
-        const res = await request(server._application)
-            .post("/api/customer/register")
-            .send({
-                name: "khalid",
-                email: "test@example.com",
-                birthday: "1997-04-25",
-            });
+  it("Create customer should return 200", async () => {
+    const res = await request("http://localhost:5000")
+      .post("/api/customer/register")
+      .send({
+        name: "khalid",
+        email: "khalid+test@example.com",
+        birthday: "1997-04-25",
+      });
 
-        // toEqual recursively checks every field of an object or array.
-        expect(res.statusCode).toEqual(200);
-    });
+    // toEqual recursively checks every field of an object or array.
+    expect(res.statusCode).toEqual(200);
+  });
 
-    it("returns bad request if firstname is missing", async () => {
-        const res = await request(server._application)
-            .post("/api/customer/register")
-            .send();
-        expect(res.statusCode).toEqual(400);
-    });
+  it("Create customer should return 400 inside response body and statuscode should be 200 with bad data:", async () => {
+    const res = await request("http://localhost:5000")
+      .post("/api/customer/register")
+      .send();
+    expect(res.statusCode).toEqual(200);
+    expect(res?.body?.statusCode).toEqual(400);
+  });
 });
